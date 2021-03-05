809611e4f6e0656ba39afa14bcebc77f219c4a22
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-05 13:55:16 +0200

    Lisätty kolme arvauskertaa.

--- DotNet/ArvaaLukuPeli/ArvaaLukuPeli/Program.cs
+++ DotNet/ArvaaLukuPeli/ArvaaLukuPeli/Program.cs
@@ -11,21 +11,25 @@ namespace ArvaaLukuPeli
             int oikea = random.Next(1, 21);
             Console.WriteLine(oikea);
 
-            Console.WriteLine("Anna arvauksesti 1-20:");
-            string syöte = Console.ReadLine();
-            int arvaus = int.Parse(syöte);
+            for (int arvausKerta = 1; arvausKerta <= 3; arvausKerta++)
+            {
+                Console.WriteLine("Anna arvauksesti 1-20:");
+                string syöte = Console.ReadLine();
+                int arvaus = int.Parse(syöte);
 
-            if (arvaus < oikea)
-            {
-                Console.WriteLine("Oikea luku on suurempi!");
-            }
-            else if (arvaus > oikea)
-            {
-                Console.WriteLine("Oikea luku on pienempi!");
-            }
-            else
-            {
-                Console.WriteLine("Arvasit oikein!");
+                if (arvaus < oikea)
+                {
+                    Console.WriteLine("Oikea luku on suurempi!");
+                }
+                else if (arvaus > oikea)
+                {
+                    Console.WriteLine("Oikea luku on pienempi!");
+                }
+                else
+                {
+                    Console.WriteLine("Arvasit oikein!");
+                    break;
+                } 
             }
 
             Console.WriteLine("Peli on päättynyt.");
