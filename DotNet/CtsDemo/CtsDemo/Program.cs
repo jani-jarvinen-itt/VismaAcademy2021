12adc061afb91da3875bbb3714fe23f8287f5f2e
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-05 10:18:40 +0200

    Lisätty CTS-tekniikan esimerkki.

---
+++ DotNet/CtsDemo/CtsDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualBasicKirjasto;
+
+namespace CtsDemo
+{
+    public class SuperLaskin : Laskin
+    {
+        public int Erotus(int a, int b)
+        {
+            return a - b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Laskin laskin = new Laskin();
+            int summa = laskin.Summa(100, 200);
+
+            Console.WriteLine(summa);
+            Console.ReadLine();
+        }
+    }
+}
