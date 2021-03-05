647598e58070c141a67fa35684aaec11c080c2dd
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-05 13:46:00 +0200

    Lisätty käyttäjän arvaaman numeron tarkistus.

--- DotNet/ArvaaLukuPeli/ArvaaLukuPeli/Program.cs
+++ DotNet/ArvaaLukuPeli/ArvaaLukuPeli/Program.cs
@@ -11,6 +11,23 @@ namespace ArvaaLukuPeli
             int oikea = random.Next(1, 21);
             Console.WriteLine(oikea);
 
+            Console.WriteLine("Anna arvauksesti 1-20:");
+            string syöte = Console.ReadLine();
+            int arvaus = int.Parse(syöte);
+
+            if (arvaus < oikea)
+            {
+                Console.WriteLine("Oikea luku on suurempi!");
+            }
+            else if (arvaus > oikea)
+            {
+                Console.WriteLine("Oikea luku on pienempi!");
+            }
+            else
+            {
+                Console.WriteLine("Arvasit oikein!");
+            }
+
             Console.WriteLine("Peli on päättynyt.");
         }
     }
