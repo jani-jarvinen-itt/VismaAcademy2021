97a1eafb7a9911386960ddb141f98e79a400a32c
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-09 13:42:47 +0200

    Lisätty esimerkki XML-kommenteista.

--- DotNet/TiedostojenLukujenSumma/TiedostojenLukujenSumma/Program.cs
+++ DotNet/TiedostojenLukujenSumma/TiedostojenLukujenSumma/Program.cs
@@ -3,15 +3,26 @@ using System.IO;
 
 namespace TiedostojenLukujenSumma
 {
+    /// <summary>
+    /// Toteuttaa tekstitiedoston numeroiden summan laskennan.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Laskee tiedoston lukujen summan.
+        /// </summary>
+        /// <param name="args">Komentoriviparametrit. Ei käytössä.</param>
         static void Main(string[] args)
         {
+            // aloitetaan tiedoston käsittely
             string tiedostoNimi = @"C:\VismaAcademy\Git\DotNet\Numerot.txt";
             int summa = 0;
             try
             {
+                // luetaan tiedosto muistiin
                 string[] rivit = File.ReadAllLines(tiedostoNimi);
+
+                // käsitellään tiedoston sisältö rivit kerrallaan
                 foreach (string rivi in rivit)
                 {
                     bool onnistui = int.TryParse(rivi, out int tulos);
@@ -39,6 +50,7 @@ namespace TiedostojenLukujenSumma
                     */
                 }
 
+                // tulostetaan lopullinen summa
                 Console.WriteLine($"Tiedoston lukujen summa on: {summa}.");
             }
             catch (Exception ex)
