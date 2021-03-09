fe7a80a853c8f75c64603dd198659ee55efacdc0
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-09 11:24:10 +0200

    Lisätty ensimmäinen versio tiedoston lukujen summan laskennasta.

---
+++ DotNet/TiedostojenLukujenSumma/TiedostojenLukujenSumma/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+namespace TiedostojenLukujenSumma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string tiedostoNimi = @"C:\VismaAcademy\Git\DotNet\Numerot.txt";
+            int summa = 0;
+            string[] rivit = File.ReadAllLines(tiedostoNimi);
+            foreach (string rivi in rivit)
+            {
+                summa += int.Parse(rivi);
+            }
+
+            Console.WriteLine($"Tiedoston lukujen summa on: {summa}.");
+        }
+    }
+}
