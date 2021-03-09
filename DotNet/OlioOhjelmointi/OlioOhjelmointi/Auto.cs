96ea14927d3842aeed7632c710415e0d1b11e837
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-09 14:42:10 +0200

    Lisätty C#-esimerkki olio-ohjelmoinnista.

---
+++ DotNet/OlioOhjelmointi/OlioOhjelmointi/Auto.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlioOhjelmointi
+{
+    public class Auto
+    {
+        public string Väri { get; set; }
+
+        public string Merkki { get; set; }
+
+        public string Malli { get; set; }
+
+        public int Huippunopeus { get; set; }
+
+        public bool MoottoriKäynnissä { get; set; }
+
+        private int Nopeus { get; set; }
+
+        public void Käynnistä()
+        {
+            MoottoriKäynnissä = true;
+        }
+
+        public void Sammuta()
+        {
+            MoottoriKäynnissä = false;
+        }
+
+        public void Kiihdytä()
+        {
+            Nopeus += 5;
+            if (Nopeus > Huippunopeus)
+            {
+                Nopeus = Huippunopeus;
+            }
+        }
+
+        public void Jarruta()
+        {
+            Nopeus -= 5;
+            if (Nopeus < 0)
+            {
+                Nopeus = 0;
+            }
+        }
+    }
+}
---
+++ DotNet/OlioOhjelmointi/OlioOhjelmointi/Urheiluauto.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlioOhjelmointi
+{
+    public class Urheiluauto : Auto
+    {
+        public bool RättikattoAlhaalla { get; set; }
+
+        public void KattoAlas()
+        {
+            RättikattoAlhaalla = true;
+        }
+    }
+}
