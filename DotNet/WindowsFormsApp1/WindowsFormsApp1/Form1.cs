66260797aa69393fc4b0759a46c5838b25c75ee4
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-10 11:59:27 +0200

    Lisätty Windows Forms -esimerkkisovellus SQL-tietokannan käsittelyyn.

---
+++ DotNet/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nimi = textBox1.Text;
+            MessageBox.Show("Moi " + nimi + "!");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'northwindDataSet.Customers' table. You can move, or remove it, as needed.
+            this.customersTableAdapter.Fill(this.northwindDataSet.Customers);
+
+        }
+    }
+}
