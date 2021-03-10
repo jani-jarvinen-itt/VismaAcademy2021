a5cc85b0b8eac6ce64d36addbcc7a8b72bf3b837
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-10 15:23:25 +0200

    Lisätty asiakkaan tilausten lukumäärän laskeminen.

--- DotNet/NorthwindEntityDemo/NorthwindEntityDemo/Program.cs
+++ DotNet/NorthwindEntityDemo/NorthwindEntityDemo/Program.cs
@@ -1,4 +1,5 @@
-﻿using NorthwindEntityDemo.Models;
+﻿using Microsoft.EntityFrameworkCore;
+using NorthwindEntityDemo.Models;
 using System;
 using System.Linq;
 
@@ -11,13 +12,16 @@ namespace NorthwindEntityDemo
             NorthwindContext konteksti = new();
 
             // LINQ-kysely
-            var suomalaiset = from c in konteksti.Customers
+            var suomalaiset = from c in konteksti.Customers.Include("Orders")
                               where c.Country == "Finland"
                               select c;
 
             foreach (Customer asiakas in suomalaiset)
             {
-                Console.WriteLine(asiakas.CompanyName);
+                string nimi = asiakas.CompanyName;
+                int tilaustenLkm = asiakas.Orders.Count();
+
+                Console.WriteLine($"Asiakkaalla {nimi} on {tilaustenLkm} kpl tilauksia.");
             }
 
             /*
