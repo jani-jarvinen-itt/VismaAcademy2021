52ee0d08b49f159c3be7557acaceadc56c3330ff
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-03-10 10:50:23 +0200

    Versio koodista using-lausekkeilla.

--- DotNet/AdoNetTesti/AdoNetTesti/Program.cs
+++ DotNet/AdoNetTesti/AdoNetTesti/Program.cs
@@ -13,32 +13,24 @@ namespace AdoNetTesti
         {
             Console.WriteLine("Avataan tietokantayhteys...");
             string yhteysmerkkijono = "Server=localhost\\SQLEXPRESS;Database=Northwind;Trusted_Connection=True;";
-            SqlConnection yhteys = new SqlConnection(yhteysmerkkijono);
-            SqlCommand komento = null;
-            SqlDataReader lukija = null;
-
-            try
+            using (SqlConnection yhteys = new SqlConnection(yhteysmerkkijono))
             {
                 yhteys.Open();
                 Console.WriteLine("Tietokantayhteys avattu.");
 
                 string sql = "SELECT * FROM Custmers WHERE Country = 'Finland'";
-                komento = new SqlCommand(sql, yhteys);
+                using (SqlCommand komento = new SqlCommand(sql, yhteys)) {
 
-                Console.WriteLine("Suoritetaan SQL-kysely...");
-                lukija = komento.ExecuteReader();
-                while (lukija.Read())
-                {
-                    string yritys = lukija["CompanyName"].ToString();
-                    Console.WriteLine(yritys);
+                    Console.WriteLine("Suoritetaan SQL-kysely...");
+                    using (SqlDataReader lukija = komento.ExecuteReader())
+                    {
+                        while (lukija.Read())
+                        {
+                            string yritys = lukija["CompanyName"].ToString();
+                            Console.WriteLine(yritys);
+                        }
+                    }
                 }
-            }
-            finally
-            {
-                // if (lukija != null) lukija.Close();
-                lukija?.Close();
-                komento?.Dispose();
-                yhteys?.Close();
             }
 
             Console.WriteLine("Suoritus päättyy.");
