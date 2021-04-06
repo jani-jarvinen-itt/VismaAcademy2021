36fa03abde6849e7d93da8af7dbb5c29624718ca
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-06 14:46:17 +0300

    Aloitettu data-integraatioprojektin koodauksia.

---
+++ DotNet/DataIntegraatioHarjoitus/DataIntegraatioHarjoitus/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataIntegraatioHarjoitus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
