1a1d1de9e74380503f20cf959b24bc1da7d88ecd
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-06 11:17:51 +0300

    Aloitettu JSON-data-integraatioharjoituksen koodaus.

---
+++ DotNet/TodoTilasto/TodoTilasto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TodoTilasto
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Aloitetaan JSON-datan lataaminen.");
+            HttpClient client = new HttpClient();
+
+            const string Url = "https://jsonplaceholder.typicode.com/todos";
+            string json = await client.GetStringAsync(Url);
+            Console.WriteLine($"Luetaan {json.Length} merkkiä dataa.");
+        }
+    }
+}
