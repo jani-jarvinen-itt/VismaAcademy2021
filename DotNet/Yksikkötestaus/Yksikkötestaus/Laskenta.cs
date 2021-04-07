0ae75f78035efa44c2f8d7d38a34713a880aa43d
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-07 15:53:15 +0300

    Lisätty yksikkötestaus-esimerkki sekä erillinen testiprojekti.

---
+++ DotNet/Yksikkötestaus/Yksikkötestaus/Laskenta.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yksikkötestaus
+{
+    public class Laskenta
+    {
+        public int Summa(int a, int b)
+        {
+            if (a > 0)
+            {
+                return a + b;
+            }
+            else
+            {
+                return a + b;
+            }
+        }
+    }
+}
---
+++ DotNet/Yksikkötestaus/YksikkötestausTests/LaskentaTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yksikkötestaus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yksikkötestaus.Tests
+{
+    [TestClass()]
+    public class LaskentaTests
+    {
+        [TestMethod()]
+        public void SummaTest()
+        {
+            int a = 100;
+            int b = 200;
+
+            Laskenta laskenta = new();
+            int summa = laskenta.Summa(a, b);
+
+            int odotettu = a + b;
+
+            Assert.AreEqual(odotettu, summa);
+        }
+
+        [TestMethod()]
+        public void SummaTest2()
+        {
+            int a = -100;
+            int b = -200;
+
+            Laskenta laskenta = new();
+            int summa = laskenta.Summa(a, b);
+
+            int odotettu = a + b;
+
+            Assert.AreEqual(odotettu, summa);
+        }
+    }
+}
