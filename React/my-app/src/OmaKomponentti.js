63bd117d858500cfc2ce476218a465ddc5e3ee41
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-15 13:13:27 +0300

    Lisätty ensimmäinen React-komponentti.

---
+++ React/my-app/src/OmaKomponentti.js
@@ -0,0 +1,13 @@
+
+function OmaKomponentti() {
+  return (
+    <div>
+        <h1>Oma Komponentti</h1>
+        <p>
+          Tämä on OmaKomponentti.js.
+        </p>
+    </div>
+  );
+}
+
+export default OmaKomponentti;
