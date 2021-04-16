316ce00c46c825502242e6058ae781fb5dd7f6a8
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-16 14:45:05 +0300

    Lisätty .NET-esimerkki kulttuurien (CultureInfo) käytöstä.

---
+++ DotNet/Kulttuurit/Kulttuurit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Kulttuurit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Syötä päivämäärä:");
+            string syöte = Console.ReadLine();
+
+            CultureInfo enUs = new CultureInfo("en-US");
+            DateTime pvm = DateTime.Parse(syöte, enUs);
+            Console.WriteLine(pvm);
+            Console.WriteLine(pvm.ToString(enUs));
+        }
+    }
+}
