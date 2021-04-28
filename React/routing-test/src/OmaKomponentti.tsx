559a2e778b9279fb91d2efb8095bc678f2c31ee0
Jani Järvinen <jani.jarvinen@intertechno.org>
2021-04-28 13:24:15 +0300

    Lisätty reititys-kirjastot sekä oma komponentti.

---
+++ React/routing-test/src/OmaKomponentti.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function OmaKomponentti() {
+  return <>
+    <h1>OmaKomponentti</h1>
+    <p>Tämä on oma komponentti.</p>
+  </>;
+}
+
+export default OmaKomponentti;
--- React/routing-test/src/index.tsx
+++ React/routing-test/src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import OmaKomponentti from './OmaKomponentti';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <OmaKomponentti />
   </React.StrictMode>,
   document.getElementById('root')
 );
